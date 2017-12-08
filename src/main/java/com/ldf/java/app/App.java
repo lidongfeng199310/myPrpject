021f0ea299220f068e02f4eeb9e42794ab4e46c2
lidongfeng199310 <1149010125@qq.com>
2017-12-08 14:39:48 +0800

    update

--- src/main/java/com/ldf/java/app/App.java
+++ src/main/java/com/ldf/java/app/App.java
@@ -3,12 +3,14 @@ package com.ldf.java.app;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
+@ImportResource(locations={"classpath:spring-applicationContext.xml"})
 public class App extends SpringBootServletInitializer{
 
     @RequestMapping("/home")
